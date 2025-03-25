afb5d7b95d03c576abc07cc5aa670c3773f8a44e
pratikpsl <pratiklad9625@gmail.com>
2025-03-25 11:34:05 +0530

    Fix image path for profile picture in Main component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -9,7 +9,7 @@ function Main() {
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-           <img src="public/images/profile_picture_icon.jpg" alt="Avatar" />
+           <img src="images/profile_picture_icon.jpg" alt="Avatar" />
         </div>
         <div className="content">
           <div className="social_icons">
