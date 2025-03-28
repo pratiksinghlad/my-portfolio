265ecf3fe72367d1bc5b2f7e8eaf1591039c307d
pratikpsl <pratiklad9625@gmail.com>
2025-03-28 22:38:51 +0530

    Add vite-plugin-compression for Brotli and Gzip support in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import checker from "vite-plugin-checker";
+import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +10,16 @@ export default defineConfig({
     checker({
       typescript: true,
       overlay: false,
+    }),
+    viteCompression({
+      algorithm: 'brotliCompress', 
+      ext: '.br',                 
+      threshold: 1024,           
+    }),
+    viteCompression({
+      algorithm: 'gzip',         
+      ext: '.gz',                  // File extension for Gzip compressed files
+      threshold: 1024,
     }),
   ],
   resolve: {
