3944fa4a15c20d04059aa5940553f33779e6ada9
pratikpsl <pratiklad9625@gmail.com>
2025-07-14 23:22:13 +0530

    Cloudflare Web Analytics support added

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
 
+// Securely inject Cloudflare Web Analytics using environment variable
+const cfToken = import.meta.env.VITE_CLOUDFLARE_TOKEN;
+if (cfToken) {
+  const script = document.createElement("script");
+  script.defer = true;
+  script.src = "https://static.cloudflareinsights.com/beacon.min.js";
+  script.setAttribute("data-cf-beacon", JSON.stringify({ token: cfToken }));
+  document.head.appendChild(script);
+}
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
